f15b50a3151e476dafa34665ba1efc0cb73d2ddc
tyronejiang <60363415+tyronejiang@users.noreply.github.com>
2021-02-10 20:20:45 -0800

    edit

--- A2/Assets/SelectionManger.cs
+++ A2/Assets/SelectionManger.cs
@@ -8,31 +8,36 @@ public class SelectionManger : MonoBehaviour
    [SerializeField] private Material highlightMaterial;
    [SerializeField] private Material defaultMaterial;
    private Transform _selection;
-
+   
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
         if (_selection != null)
         {
             var selectionRenderer = _selection.GetComponent<Renderer>();
-            selectionRenderer.material = defaultMaterial;
-            _selection = null;
+            if (selectionRenderer != null)
+            {
+                selectionRenderer.material = defaultMaterial;
+            }
         }
         var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        RaycastHit hit;
-        if(Physics.Raycast(ray, out hit))
+        _selection = null;
+       
+        if(Physics.Raycast(ray, out var hit))
         {
             var selection = hit.transform;
             if (selection.CompareTag (selectableTag))
             {
-                var selectionRenderer = selection.GetComponent<Renderer>();
-                if (selectionRenderer != null)
-                {
-                    selectionRenderer.material = highlightMaterial;
-                }
                 _selection = selection;
             }
-            
+        }
+        if (_selection != null)
+        {
+            var selectionRenderer = _selection.GetComponent<Renderer>();
+            if (selectionRenderer != null)
+            {
+                selectionRenderer.material = highlightMaterial;
+            }    
         }
     }
 }
